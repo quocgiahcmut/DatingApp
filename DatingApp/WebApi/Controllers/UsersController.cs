66e1252e119cab779ae3f6be9eac2cfb1ee715b0
Quoc Gia <67970731+quocgiahcmut@users.noreply.github.com>
2023-06-12 17:27:12 +0700

    first commit

---
+++ DatingApp/WebApi/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Data;
+using WebApi.Entities;
+
+namespace WebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UsersController : ControllerBase
+	{
+		private readonly ApplicationDbContext _context;
+
+		public UsersController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		[HttpGet]
+		public ActionResult<IEnumerable<AppUser>> GetAllUsers()
+		{
+			var users = _context.Users.ToList();
+
+			return Ok(users);
+		}
+
+		[HttpGet("{id}")]
+		public ActionResult<AppUser> GetUser(int id)
+		{
+			var user = _context.Users.Find(id);
+
+			return Ok(user);
+		}
+	}
+}
---
+++ DatingApp/WebApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Entities;
+
+namespace WebApi.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions options) : base(options)
+		{
+		}
+
+		public DbSet<AppUser> Users { get; set; }
+	}
+}
