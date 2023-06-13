649ccdb9b30645424a3e2be42b5e9fd8b353ee00
Quoc Gia <67970731+quocgiahcmut@users.noreply.github.com>
2023-06-13 17:18:13 +0700

    ui login and register
    add nav bar
    add home page

--- DatingApp/Client/src/app/app.module.ts
+++ DatingApp/Client/src/app/app.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent, NavComponent, HomeComponent],
+    imports: [
+        BrowserAnimationsModule,
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        BsDropdownModule.forRoot(),
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ DatingApp/Client/src/app/services/account.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, map } from 'rxjs';
+import { User } from '../models/user';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class AccountService {
+    baseUrl = 'https://localhost:7000/api/';
+    private currentUserSource = new BehaviorSubject<User | null>(null);
+    currentUser$ = this.currentUserSource.asObservable();
+
+    constructor(private http: HttpClient) {}
+
+    login(model: any) {
+        return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
+            map((res: User) => {
+                const user = res;
+                if (user) {
+                    localStorage.setItem('user', JSON.stringify(user));
+                    this.currentUserSource.next(user);
+                }
+            })
+        );
+    }
+
+    setCurrentUser(user: User) {
+        this.currentUserSource.next(user);
+    }
+
+    logout() {
+        localStorage.removeItem('user');
+        this.currentUserSource.next(null);
+    }
+}
