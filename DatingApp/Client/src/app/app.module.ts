edfc04258ed6de39bfbccd052a5bad5099979764
Quoc Gia <67970731+quocgiahcmut@users.noreply.github.com>
2023-06-30 00:55:36 +0700

    add snipper
    but it not working

--- DatingApp/Client/src/app/app.module.ts
+++ DatingApp/Client/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
@@ -21,6 +21,7 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
 import { MemberCardComponent } from './components/members/member-card/member-card.component';
 import { JwtInterceptor } from './interceptor/jwt.interceptor';
 import { MemberEditComponent } from './pages/members/member-edit/member-edit.component';
+import { LoadingInterceptor } from './interceptor/loading.interceptor';
 
 @NgModule({
     declarations: [
@@ -50,7 +51,9 @@ import { MemberEditComponent } from './pages/members/member-edit/member-edit.com
     providers: [
         { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
         { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true },
     ],
     bootstrap: [AppComponent],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
